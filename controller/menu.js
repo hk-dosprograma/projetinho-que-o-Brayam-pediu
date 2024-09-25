f72db47abde18c0eb671136365693876ecd96e1f
hector dos programas <hbonnospok@gmail.com>
2024-09-25 17:19:36 -0300

    barraMenuAtt

---
+++ controller/menu.js
@@ -0,0 +1,12 @@
+let menu = document.getElementById('menu')
+let barra_um = document.getElementById('barra_um')
+let barra_dois = document.getElementById('barra_dois')
+let barra_tres = document.getElementById('barra_tres')
+export function trocarMenu(){
+    if(menu.style.left === '0vw'){
+        menu.style.left = '-100vw';
+        barra_um.style
+    } else {
+        menu.style.left = '0vw';
+    }
+}
--- controller/projetos.js
+++ controller/projetos.js
@@ -1,5 +1,7 @@
 import { trocarBanner } from "./banners.js";
+import { trocarMenu } from "./menu.js";
 
+window.trocarMenu =trocarMenu;
 window.trocarBanner = trocarBanner;
 
 document.addEventListener("DOMContentLoaded", function () {
